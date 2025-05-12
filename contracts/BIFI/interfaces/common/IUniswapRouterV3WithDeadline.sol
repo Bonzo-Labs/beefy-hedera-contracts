8a51e762e715ddfcb4fd45ea659c4b0d9829d172
khemraj <khemraj@khemrajs-MacBook-Air.local>
2025-05-12 13:16:23 +0530

    chore: added saucerswap strategy setup

--- contracts/BIFI/interfaces/common/IUniswapRouterV3WithDeadline.sol
+++ contracts/BIFI/interfaces/common/IUniswapRouterV3WithDeadline.sol
@@ -4,6 +4,20 @@ pragma solidity ^0.8.0;
 
 
 interface IUniswapRouterV3WithDeadline {
+    struct MintParams {
+        address token0;
+        address token1;
+        uint24 fee;
+        int24 tickLower;
+        int24 tickUpper;
+        uint256 amount0Desired;
+        uint256 amount1Desired;
+        uint256 amount0Min;
+        uint256 amount1Min;
+        address recipient;
+        uint256 deadline;
+    }
+
     struct ExactInputSingleParams {
         address tokenIn;
         address tokenOut;
@@ -61,4 +75,8 @@ interface IUniswapRouterV3WithDeadline {
     /// @param params The parameters necessary for the multi-hop swap, encoded as `ExactOutputParams` in calldata
     /// @return amountIn The amount of the input token
     function exactOutput(ExactOutputParams calldata params) external payable returns (uint256 amountIn);
+
+    function mint(MintParams memory params) external payable returns (uint256 tokenId, uint128 liquidity, uint256 amount0, uint256 amount1);
+
+    function getAmountsOut(uint256 amountIn, bytes memory path) external view returns (uint256[] memory amounts);
 }
---
+++ contracts/BIFI/strategies/SaucerSwap/StrategyCommonSaucerSwap.sol
@@ -0,0 +1,311 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin-4/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin-4/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "../../interfaces/common/IUniswapRouterV3WithDeadline.sol";
+import "../../interfaces/common/IUniswapV3Pool.sol";
+import "../../interfaces/common/IRewardPool.sol";
+import "../Common/StratFeeManagerInitializable.sol";
+import "../../utils/GasFeeThrottler.sol";
+import "../../utils/UniswapV3Utils.sol";
+
+contract StrategyCommonSaucerSwap is StratFeeManagerInitializable, GasFeeThrottler {
+    using SafeERC20 for IERC20;
+    using UniswapV3Utils for bytes;
+
+    // Tokens used
+    address public native;
+    address public output;
+    address public want;
+    address public lpToken0;
+    address public lpToken1;
+
+    // Third party contracts
+    address public rewardPool;
+    address public saucerSwapRouter;
+
+    bool public harvestOnDeposit;
+    uint256 public lastHarvest;
+
+    // Routes
+    address[] public outputToNativeRoute;
+    address[] public outputToLp0Route;
+    address[] public outputToLp1Route;
+    uint24[] public outputToNativeFee;
+    uint24[] public outputToLp0Fee;
+    uint24[] public outputToLp1Fee;
+
+    event StratHarvest(address indexed harvester, uint256 wantHarvested, uint256 tvl);
+    event Deposit(uint256 tvl);
+    event Withdraw(uint256 tvl);
+    event ChargedFees(uint256 callFees, uint256 beefyFees, uint256 strategistFees);
+
+    function initialize(
+        address _want,
+        address _rewardPool,
+        address _saucerSwapRouter,
+        address[] calldata _outputToNativeRoute,
+        address[] calldata _outputToLp0Route,
+        address[] calldata _outputToLp1Route,
+        uint24[] calldata _outputToNativeFee,
+        uint24[] calldata _outputToLp0Fee,
+        uint24[] calldata _outputToLp1Fee,
+        CommonAddresses calldata _commonAddresses
+    ) public initializer {
+        __StratFeeManager_init(_commonAddresses);
+        want = _want;
+        rewardPool = _rewardPool;
+        saucerSwapRouter = _saucerSwapRouter;
+
+        output = _outputToNativeRoute[0];
+        native = _outputToNativeRoute[_outputToNativeRoute.length - 1];
+        outputToNativeRoute = _outputToNativeRoute;
+        outputToNativeFee = _outputToNativeFee;
+
+        // setup lp routing
+        lpToken0 = IUniswapV3Pool(want).token0();
+        require(_outputToLp0Route[0] == output, "outputToLp0Route[0] != output");
+        require(_outputToLp0Route[_outputToLp0Route.length - 1] == lpToken0, "outputToLp0Route[last] != lpToken0");
+        outputToLp0Route = _outputToLp0Route;
+        outputToLp0Fee = _outputToLp0Fee;
+
+        lpToken1 = IUniswapV3Pool(want).token1();
+        require(_outputToLp1Route[0] == output, "outputToLp1Route[0] != output");
+        require(_outputToLp1Route[_outputToLp1Route.length - 1] == lpToken1, "outputToLp1Route[last] != lpToken1");
+        outputToLp1Route = _outputToLp1Route;
+        outputToLp1Fee = _outputToLp1Fee;
+
+        _giveAllowances();
+    }
+
+    // puts the funds to work
+    function deposit() public whenNotPaused {
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+
+        if (wantBal > 0) {
+            IRewardPool(rewardPool).stake(wantBal);
+            emit Deposit(balanceOf());
+        }
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == vault, "!vault");
+
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+
+        if (wantBal < _amount) {
+            IRewardPool(rewardPool).withdraw(_amount - wantBal);
+            wantBal = IERC20(want).balanceOf(address(this));
+        }
+
+        if (wantBal > _amount) {
+            wantBal = _amount;
+        }
+
+        if (tx.origin != owner() && !paused()) {
+            uint256 withdrawalFeeAmount = wantBal * withdrawalFee / WITHDRAWAL_MAX;
+            wantBal = wantBal - withdrawalFeeAmount;
+        }
+
+        IERC20(want).safeTransfer(vault, wantBal);
+
+        emit Withdraw(balanceOf());
+    }
+
+    function beforeDeposit() external virtual override {
+        if (harvestOnDeposit) {
+            require(msg.sender == vault, "!vault");
+            _harvest(tx.origin);
+        }
+    }
+
+    function harvest() external gasThrottle virtual {
+        _harvest(tx.origin);
+    }
+
+    function harvest(address callFeeRecipient) external gasThrottle virtual {
+        _harvest(callFeeRecipient);
+    }
+
+    function managerHarvest() external onlyManager {
+        _harvest(tx.origin);
+    }
+
+    // compounds earnings and charges performance fee
+    function _harvest(address callFeeRecipient) internal whenNotPaused {
+        IRewardPool(rewardPool).getReward();
+        uint256 outputBal = IERC20(output).balanceOf(address(this));
+        if (outputBal > 0) {
+            chargeFees(callFeeRecipient);
+            addLiquidity();
+            uint256 wantHarvested = balanceOfWant();
+            deposit();
+
+            lastHarvest = block.timestamp;
+            emit StratHarvest(msg.sender, wantHarvested, balanceOf());
+        }
+    }
+
+    // performance fees
+    function chargeFees(address callFeeRecipient) internal {
+        IFeeConfig.FeeCategory memory fees = getFees();
+        uint256 toNative = IERC20(output).balanceOf(address(this)) * fees.total / DIVISOR;
+        
+        bytes memory path = UniswapV3Utils.routeToPath(outputToNativeRoute, outputToNativeFee);
+        UniswapV3Utils.swap(saucerSwapRouter, path, toNative);
+
+        uint256 nativeBal = IERC20(native).balanceOf(address(this));
+
+        uint256 callFeeAmount = nativeBal * fees.call / DIVISOR;
+        IERC20(native).safeTransfer(callFeeRecipient, callFeeAmount);
+
+        uint256 beefyFeeAmount = nativeBal * fees.beefy / DIVISOR;
+        IERC20(native).safeTransfer(beefyFeeRecipient, beefyFeeAmount);
+
+        uint256 strategistFeeAmount = nativeBal * fees.strategist / DIVISOR;
+        IERC20(native).safeTransfer(strategist, strategistFeeAmount);
+
+        emit ChargedFees(callFeeAmount, beefyFeeAmount, strategistFeeAmount);
+    }
+
+    // Adds liquidity to AMM and gets more LP tokens.
+    function addLiquidity() internal {
+        uint256 outputHalf = IERC20(output).balanceOf(address(this)) / 2;
+        
+        if (lpToken0 != output) {
+            bytes memory path = UniswapV3Utils.routeToPath(outputToLp0Route, outputToLp0Fee);
+            UniswapV3Utils.swap(saucerSwapRouter, path, outputHalf);
+        }
+
+        if (lpToken1 != output) {
+            bytes memory path = UniswapV3Utils.routeToPath(outputToLp1Route, outputToLp1Fee);
+            UniswapV3Utils.swap(saucerSwapRouter, path, outputHalf);
+        }
+
+        uint256 lp0Bal = IERC20(lpToken0).balanceOf(address(this));
+        uint256 lp1Bal = IERC20(lpToken1).balanceOf(address(this));
+
+        // Add liquidity using SaucerSwap V3 router
+        IUniswapRouterV3WithDeadline.MintParams memory _params = IUniswapRouterV3WithDeadline.MintParams({
+            token0: lpToken0,
+            token1: lpToken1,
+            fee: outputToLp0Fee[0], // Use the fee from the first pool
+            tickLower: -887220, // Default to full range
+            tickUpper: 887220,  // Default to full range
+            amount0Desired: lp0Bal,
+            amount1Desired: lp1Bal,
+            amount0Min: 1,
+            amount1Min: 1,
+            recipient: address(this),
+            deadline: block.timestamp
+        });
+
+        IUniswapRouterV3WithDeadline(saucerSwapRouter).mint(_params);
+    }
+
+    // calculate the total underlaying 'want' held by the strat.
+    function balanceOf() public view returns (uint256) {
+        return balanceOfWant() + balanceOfPool();
+    }
+
+    // it calculates how much 'want' this contract holds.
+    function balanceOfWant() public view returns (uint256) {
+        return IERC20(want).balanceOf(address(this));
+    }
+
+    // it calculates how much 'want' the strategy has working in the farm.
+    function balanceOfPool() public view returns (uint256) {
+        return IRewardPool(rewardPool).balanceOf(address(this));
+    }
+
+    // returns rewards unharvested
+    function rewardsAvailable() public view returns (uint256) {
+        return IRewardPool(rewardPool).earned(address(this));
+    }
+
+    // native reward amount for calling harvest
+    function callReward() public view returns (uint256) {
+        IFeeConfig.FeeCategory memory fees = getFees();
+        uint256 outputBal = rewardsAvailable();
+        uint256 nativeOut;
+        if (outputBal > 0) {
+            bytes memory path = UniswapV3Utils.routeToPath(outputToNativeRoute, outputToNativeFee);
+            nativeOut = IUniswapRouterV3WithDeadline(saucerSwapRouter).getAmountsOut(outputBal, path)[path.length - 1];
+        }
+
+        return nativeOut * fees.total / DIVISOR * fees.call / DIVISOR;
+    }
+
+    function setHarvestOnDeposit(bool _harvestOnDeposit) external onlyManager {
+        harvestOnDeposit = _harvestOnDeposit;
+
+        if (harvestOnDeposit) {
+            setWithdrawalFee(0);
+        } else {
+            setWithdrawalFee(10);
+        }
+    }
+
+    function setShouldGasThrottle(bool _shouldGasThrottle) external onlyManager {
+        shouldGasThrottle = _shouldGasThrottle;
+    }
+
+    // called as part of strat migration. Sends all the available funds back to the vault.
+    function retireStrat() external {
+        require(msg.sender == vault, "!vault");
+
+        IRewardPool(rewardPool).withdraw(balanceOfPool());
+
+        uint256 wantBal = IERC20(want).balanceOf(address(this));
+        IERC20(want).transfer(vault, wantBal);
+    }
+
+    // pauses deposits and withdraws all funds from third party systems.
+    function panic() public onlyManager {
+        pause();
+        IRewardPool(rewardPool).withdraw(balanceOfPool());
+    }
+
+    function pause() public onlyManager {
+        _pause();
+
+        _removeAllowances();
+    }
+
+    function unpause() external onlyManager {
+        _unpause();
+
+        _giveAllowances();
+
+        deposit();
+    }
+
+    function _giveAllowances() internal {
+        IERC20(want).safeApprove(rewardPool, type(uint).max);
+        IERC20(output).safeApprove(saucerSwapRouter, type(uint).max);
+        IERC20(lpToken0).safeApprove(saucerSwapRouter, type(uint).max);
+        IERC20(lpToken1).safeApprove(saucerSwapRouter, type(uint).max);
+    }
+
+    function _removeAllowances() internal {
+        IERC20(want).safeApprove(rewardPool, 0);
+        IERC20(output).safeApprove(saucerSwapRouter, 0);
+        IERC20(lpToken0).safeApprove(saucerSwapRouter, 0);
+        IERC20(lpToken1).safeApprove(saucerSwapRouter, 0);
+    }
+
+    function outputToNative() external view returns (address[] memory) {
+        return outputToNativeRoute;
+    }
+
+    function outputToLp0() external view returns (address[] memory) {
+        return outputToLp0Route;
+    }
+
+    function outputToLp1() external view returns (address[] memory) {
+        return outputToLp1Route;
+    }
+}
